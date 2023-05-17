1e705b4685c959c914f73b23764741e4ec8af586
Eric L <eriliu@ucdavis.edu>
2023-05-17 11:05:33 -0700

    main_no_modal.py: Remove trailing whitespace

--- main_no_modal.py
+++ main_no_modal.py
@@ -71,34 +71,34 @@ def generate_file(
     # call openai api with this prompt
     filecode = generate_response(
         f"""You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-        
+
     the app is: {prompt}
 
     the files we have decided to generate are: {filepaths_string}
 
     the shared dependencies (like filenames and variable names) we have decided on are: {shared_dependencies}
-    
+
     only write valid code for the given filepath and file type, and return only the code.
     do not add any other explanation, only return valid code for that file type.
     """,
         f"""
-    We have broken up the program into per-file generation. 
-    Now your job is to generate only the code for the file {filename}. 
+    We have broken up the program into per-file generation.
+    Now your job is to generate only the code for the file {filename}.
     Make sure to have consistent filenames if you reference other files we are also generating.
-    
-    Remember that you must obey 3 things: 
+
+    Remember that you must obey 3 things:
        - you are generating code for the file {filename}
        - do not stray from the names of the files and the shared dependencies we have decided on
        - MOST IMPORTANT OF ALL - the purpose of our app is {prompt} - every line of code you generate must be valid code. Do not include code fences in your response, for example
-    
+
     Bad response:
-    ```javascript 
+    ```javascript
     console.log("hello world")
     ```
-    
+
     Good response:
     console.log("hello world")
-    
+
     Begin generating the code now.
 
     """,
@@ -124,10 +124,10 @@ def main(prompt, directory=generatedDir, file=None):
     # call openai api with this prompt
     filepaths_string = generate_response(
         """You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-        
+
     When given their intent, create a complete, exhaustive list of filepaths that the user would write to make the program.
-    
-    only list the filepaths you would write, and return them as a python list of strings. 
+
+    only list the filepaths you would write, and return them as a python list of strings.
     do not add any other explanation, only return a python list of strings.
     """,
         prompt,
@@ -160,13 +160,13 @@ def main(prompt, directory=generatedDir, file=None):
             # understand shared dependencies
             shared_dependencies = generate_response(
                 """You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-                
+
             In response to the user's prompt:
 
             ---
             the app is: {prompt}
             ---
-            
+
             the files we have decided to generate are: {filepaths_string}
 
             Now that we have a list of files, we need to understand what dependencies they share.
