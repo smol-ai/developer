c52b14b1d5b1b74d886f08d9914e7f43437f609d
Silen Naihin <silen.naihin@gmail.com>
2023-07-10 21:36:25 -0400

    add reports, consolidate, update benchmark files

--- agbenchmark/benchmarks.py
+++ agbenchmark/benchmarks.py
@@ -7,19 +7,20 @@ from typing import Tuple
 
 def run_specific_agent(task: str) -> Tuple[str, int]:
     # Construct the command
-    command = ['python', 'main_no_modal.py', task]
+    command = ["python", "main_no_modal.py", task]
     subprocess.run(command, text=True)
+
 
 def execute_generated_files():
     # Navigate to generated directory
-    os.chdir('generated')
+    os.chdir("generated")
 
     # Iterate over every .txt file in the directory
-    for file_name in glob.glob('*.txt'):
-        with open(file_name, 'r') as file:
+    for file_name in glob.glob("../*.txt"):
+        with open(file_name, "r") as file:
             python_code = file.read()
-            python_code = python_code.replace('```python', '')
-            python_code = python_code.replace('```', '')
+            python_code = python_code.replace("```python", "")
+            python_code = python_code.replace("```", "")
             # Execute the code in the .txt file
             exec(python_code)
 
